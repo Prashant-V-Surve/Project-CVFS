2e302ae98bd207158c3918e50f4d0ae80b3efa8e
Prashant-V-Surve <prashantsurve110@gmail.com>
2025-11-05 23:46:54 +0530

    Add files via upload

---
+++ Customized_Virtual_File_System_(CVFS)/program704.cpp
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////
+//     
+//  File name :     program704.cpp
+//  Descreption :   Customized Virtual File System(CVFS).
+//  Author :        Prashant V. Surve
+//  Date :          10/08/2025
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+//#include<sting.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    cout<<"Jai Ganesh...\n";
+    
+    return 0;
+}
---
+++ Customized_Virtual_File_System_(CVFS)/program714.cpp
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////
+//     
+//  File name :     program714.cpp
+//  Descreption :   Customized Virtual File System(CVFS).
+//  Author :        Prashant V. Surve
+//  Date :          11/08/2025
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  system("ls");              //for clear command 
+
+
+    return 0;
+}  //end of main
